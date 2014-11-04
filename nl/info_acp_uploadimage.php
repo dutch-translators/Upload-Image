825e6c1f4bc3fc3f00a071c72ba114425d764cf0
BianCionaa <michelheusschen@gmail.com>
2014-11-04 19:17:32 +0100

    Create info_acp_uploadimage.php

---
+++ nl/info_acp_uploadimage.php
@@ -0,0 +1,27 @@
+<?php
+/**
+*
+* @package Upload Image
+* @copyright (c) 2014 John Peskens (http://ForumHulp.com)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACP_IMAGE_MANAGEMENT'			=> 'Upload Images',
+	'ACP_IMAGE_TITLE'				=> 'Upload Image',
+
+	'ACP_IMAGE_DELETE_ERRORED'		=> 'Er ging iets mis.',
+	'ACP_IMAGE_DELETE_SUCCESS'		=> 'Afbeelding verwijderd',
+	'ACP_IMAGE_DELETED_LOG'			=> '<strong>Afbeelding verwijderd</strong><br />» %1s',
+));
